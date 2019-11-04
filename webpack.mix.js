52720db7bed250ad66bd0f4ec5761b06d608e1a6
Lê Đình Thắng <ledinhthang102@gmail.com>
2019-11-04 23:47:18 +0700

    Fix mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+   .sass("resources/sass/app.scss", "public/css")
+   .copyDirectory('resources/metronic4.5.6', 'public/template/admin')
+   .options({
+      processCssUrls: false
+   })
+   // Admin custom
+   .js("resources/js/admin.js", "public/js/admin")
+   .sass("resources/sass/admin.scss", "public/css/admin");
